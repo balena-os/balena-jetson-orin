f45146418baa39cd8349f7c7fe02ff4e789fcf6c
Alexandru Costache <alexandru@balena.io>
2024-07-01 13:29:20 +0000

    recipes-bsp/uefi: Update integration for L4T 36.3
    ... and also switch from building edk2-firmware-tegra
    in Yocto to building it in a container. This because
    the upstream integration is made to work with Poky
    Scarthgap, which is not yet available in meta-balena.
    
    We also updated our balenaOS integration patches for
    all boards to allow most rollbacks to work when migrating
    from Jetpack 5 to Jetpack 6.
    
    The tested rollback scenarios are in the following order, without re-flashing between steps:
    
    - Perform HUP from Jetpack 5 L4T 35.5.0 and interrupt UEFI capsule update in firmware
    - Perform HUP from Jetpack 5 L4T 35.5.0 and trigger rollback altboot
    - Perform HUP from Jetpack 5 L4T 35.5.0 and trigger rollback health, using the same device
    - Perform HUP to L4T 36.3
    - Perform one more HUP to L4T 36.3
    
    Signed-off-by: Alexandru Costache <alexandru@balena.io>

---
+++ layers/meta-balena-jetson/recipes-bsp/uefi/files/build.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# This script patches the official EDK2 sources
+# from github with balenaOS specific changes
+# and then builds the UEFI firmware and bootloader
+
+declare -A device_specific_patches
+
+device_specific_patches["jetson-agx-orin-devkit"]="0001-AGX-Orin-32GB-Integrate-with-balenaOS-on-L4T-36.3.patch"
+device_specific_patches["jetson-orin-nano-devkit-nvme"]="0001-Orin-Nano-Integrate-with-balenaOS-on-L4T-36.3.patch"
+device_specific_patches["jetson-orin-nx-xavier-nx-devkit"]="0001-Orin-NX-16GB-Integrate-with-balenaOS-on-L4T-36.3.patch"
+device_specific_patches["jetson-orin-nx-seeed-j4012"]="0001-Seeed-J4012-Integrate-with-balenaOS-on-L4T-36.3.patch"
+device_specific_patches["jetson-orin-nx-seeed-j3010"]="0001-Seeed-J3010-Integrate-with-balenaOS-on-L4T-36.3.patch"
+
+edk2_patch="0001-edk2-Disable-network-boot-and-allow-UEFI-capsule-dow.patch"
+
+edk2_nvidia_patches=( "0001-edk2-nvidia-Add-changes-for-balenaOS-integration.patch " \
+			"0001-edk2-nvidia-Remove-pva-fw-from-required-list.patch" \
+			"0001-feat-add-a-null-version-of-the-FwVariableLib.patch" \
+			"0002-fix-don-t-try-to-erase-early-vars-partition-in-Jetso.patch" \
+			"0003-fix-reset-the-meas-buffer-after-computing-the-first-.patch" )
+
+cd /build/nvidia-uefi/edk2 && \
+    git reset --hard HEAD && \
+    git apply ${edk2_patch} && \
+    cd /build/nvidia-uefi/edk2-nvidia && \
+    git reset --hard HEAD && \
+    for edk2_nvidia_patch in ${edk2_nvidia_patches[@]}
+    do
+        git apply "${edk2_nvidia_patch}" ;
+	echo "Applied ${edk2_nvidia_patch}"
+    done
+
+    echo "Device type is ${DEVICE_TYPE}"
+    for device_specific_patch in ${!device_specific_patches[@]}
+    do
+        if [[ "${DEVICE_TYPE}" == "${device_specific_patch}" ]]; then
+		echo "Applying ${device_specific_patches[${DEVICE_TYPE}]} for ${DEVICE_TYPE}"
+		git apply ${device_specific_patches[${DEVICE_TYPE}]}
+
+	fi
+    done
+
+# Trigger firmware and bootloader build, that is uefi_jetson.bin
+# and BOOTAA64.efi
+/build/nvidia-uefi/edk2-nvidia/Platform/NVIDIA/Jetson/build.sh
